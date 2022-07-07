0cbc0068e5cf5fa42cca09b80aca9b74a3ce6597
NguyenQuangThanh0412 <1811061696@hunre.edu.vn>
2022-07-07 23:17:44 +0700

    update

--- media/media/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import authReducer from './AuthReducer'
-
-import postReducer from "./PostReducer";
-
-
-
-export const reducers = combineReducers({authReducer, postReducer})
